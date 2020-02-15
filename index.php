04163d89587a4d1fd74ba68b354955552db53a3e
j-alizadeh <jalizade3d@yahoo.com>
2020-02-15 18:01:08 +0100

    English tutorial

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<meta name="description" content="Free Web tutorials">
-	<meta name="keywords" content="HTML,CSS,XML,JavaScript">
-	<meta name="author" content="John Doe">
+	<meta name="description" content="Download Instagram's Live Video / Convert Links">
+	<meta name="keywords" content="download,instagram,live,video">
+	<meta name="author" content="Javad Alizadeh">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Download Instagram's Live Video / Convert Links</title>
 	<style type="text/css">
@@ -20,6 +20,47 @@
 	Paste data here:
 	<br/>
 	<textarea name="data"><?php if(isset($_POST['data'])) { echo ($_POST['data']); } ?></textarea>
+	
+	<a href="https://virgool.io/@jalizadeh/%DA%86%DA%AF%D9%88%D9%86%D9%87-%D9%84%D8%A7%DB%8C%D9%88-%D8%A7%DB%8C%D9%86%D8%B3%D8%AA%D8%A7%DA%AF%D8%B1%D8%A7%D9%85-%D8%B1%D8%A7-%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF-%DA%A9%D9%86%D9%85-wnujr1scyb7b"><svg width="32px" height="32px" viewBox="0 0 204 304">
+	<style type="text/css">
+	                    .st0{fill:url(#Shape_1_);}
+	                    .st1{filter:url(#filter-3);}
+	                    .st2{fill:#FFFFFF;}
+	                  </style>
+	<filter filterUnits="objectBoundingBox" height="200%" id="filter-3" width="200%" x="-50%" y="-50%">
+	<feOffset dx="0" dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset>
+	<feGaussianBlur in="shadowOffsetOuter1" result="shadowBlurOuter1" stdDeviation="1.5"></feGaussianBlur>
+	<feColorMatrix in="shadowBlurOuter1" type="matrix" values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.5 0"></feColorMatrix>
+	</filter>
+	<linearGradient id="Shape_1_" gradientUnits="userSpaceOnUse" x1="-892.5724" y1="1149.3007" x2="-891.5724" y2="1149.5884" gradientTransform="matrix(203.0965 0 0 -275.684 181280.5781 317044.6562)">
+	<stop offset="0" class="page_speed_1978385625"></stop>
+	<stop offset="1" class="page_speed_1405183599"></stop>
+	</linearGradient>
+	<path id="Shape" class="st0" d="M100.2,303.7c-16.2-11.1-32.1-22-48.5-33.2c9.9-13.9,19.7-27.4,29-40.4
+	                                        c-10.1-4.2-20.5-7.2-29.4-12.7c-35.1-21.5-54-52.6-51-94.4c3.1-43.4,26.3-73.9,66.5-88.4c67.8-24.4,125,23.1,134.4,75.5
+	                                        c4.1,22.8,1.9,45.2-10.4,65.1c-12.4,20.1-26.2,39.4-39.8,58.7c-15.6,22-31.7,43.7-47.5,65.5C102.6,300.6,101.7,301.8,100.2,303.7z"></path>
+	<g id="Fill-1" transform="translate(113.070063, 101.070415) rotate(210.000000) translate(-113.070063, -101.070415) ">
+	<g class="st1">
+	<path id="path-2_2_" d="M164.9,183.5c-11,0-21.8,0-32.9,0c0.2-9.5,0.4-49.1,0.6-58c-6,1.2-11.8,3.1-17.6,3.4
+	                                              c-23,1.2-41.5-7.3-53.3-27.5c-12.3-21-11.2-42.5,2.8-61.8c23.6-32.7,65.1-28.8,85.9-7.6c9.1,9.2,15.2,20.3,15.7,33.4
+	                                              c0.6,13.2,0.3,26.4,0.1,39.7"></path>
+	</g>
+	<g>
+	<path id="path-2_1_" class="st2" d="M166.6,182.5c-11,0-21.8,0-32.9,0c0.2-9.5,0.4-49.1,0.6-58c-6,1.2-11.8,3.1-17.6,3.4
+	                                              c-23,1.2-41.5-7.3-53.3-27.5c-12.3-21-11.2-42.5,2.8-61.8c23.6-32.7,65.1-28.8,85.9-7.6c9.1,9.2,15.2,20.3,15.7,33.4
+	                                              c0.6,13.2,0.3,26.4,0.1,39.7"></path>
+	</g>
+	</g>
+	</svg></a>
+
+	<a href="https://medium.com/@jalizadeh/download-instagrams-live-videos-with-highest-quality-37d16c923261"  target="blank"><svg width="32" height="32" viewBox="5 5 35 35"><path d="M5 40V5h35v35H5zm8.56-12.63c0 .56-.03.69-.32 1.03L10.8 31.4v.4h6.97v-.4L15.3 28.4c-.29-.34-.34-.5-.34-1.03v-8.95l6.13 13.36h.71l5.26-13.36v10.64c0 .3 0 .35-.19.53l-1.85 1.8v.4h9.2v-.4l-1.83-1.8c-.18-.18-.2-.24-.2-.53V15.94c0-.3.02-.35.2-.53l1.82-1.8v-.4h-6.47l-4.62 11.55-5.2-11.54h-6.8v.4l2.15 2.63c.24.3.29.37.29.77v10.35z"></path></svg></a>
+
+	<a href="https://github.com/jalizadeh/Download-Instagram-Live-Video" target="blank">
+  		<svg height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true">
+  			<path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
+  		</svg>
+	</a>
+
 	<br/>
 	<input name="submit" type="submit" value="Process Links" />
 </form>
